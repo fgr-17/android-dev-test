6c9af714a1d4bfbfe8c049a408ccf2d49953744d
Federico Roux <globant-froux@hme.com>
2024-05-14 16:22:25 -0300

    booleans

---
+++ KotlinBasics/app/src/main/java/eu/tutorials/kotlinbasics/BooleanPractice.kt
@@ -0,0 +1,10 @@
+package eu.tutorials.kotlinbasics
+
+fun main() {
+    var myTrue = true
+    var myFalse = false
+
+    println(myTrue || myFalse)
+    println(myTrue && myFalse)
+    println(!myTrue)
+}
