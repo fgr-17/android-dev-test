11c4baa7fff87654eadf001df11f54588ea474ec
Federico Roux <globant-froux@hme.com>
2024-05-14 16:07:32 -0300

    mutable and immutable vars, data types

---
+++ KotlinBasics/app/src/main/java/eu/tutorials/kotlinbasics/Basics.kt
@@ -0,0 +1,15 @@
+package eu.tutorials.kotlinbasics
+
+fun main () {
+    println("Hello World")
+
+    // values are immutable objects
+    val constantNumber = 1
+    var mutableString = "mutable string"
+    // Specify the type explicitly
+    var myAge : Byte = 36
+    myAge = 39
+
+    println(myAge)
+    var newVar = "Hi"
+}
