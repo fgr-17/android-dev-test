d85b2dbeb37b14370f8ca93afbe9eebea5b4c70c
Federico Roux <rouxfederico@gmail.com>
2024-05-21 10:13:51 -0300

    update of style

--- KotlinBasics/app/src/main/java/eu/tutorials/kotlinbasics/Functions.kt
+++ KotlinBasics/app/src/main/java/eu/tutorials/kotlinbasics/Functions.kt
@@ -3,15 +3,15 @@ package eu.tutorials.kotlinbasics
 fun main(){
 
     // Call Function
-    makeCoffee(sugarCount = 1, name = "fede")
-    makeCoffee(sugarCount = 2, name = "pepe")
-    makeCoffee(sugarCount = 30, "Santino")
-    makeCoffee(sugarCount = 0, "pepito")
-    makeCoffee(sugarCount = -1, "esculapio")
+    makeCoffee(1, "fede")
+    makeCoffee(2, "pepe")
+    makeCoffee(30, "Santino")
+    makeCoffee(0, "pepito")
+    makeCoffee(-1, "esculapio")
 }
 
 // Define Function
-fun makeCoffee(sugarCount : Int, name : String){
+fun makeCoffee(sugarCount:Int, name:String){
     if ( sugarCount == 1 ) {
         println("Coffee with 1 spoon of sugar for $name")
     }
