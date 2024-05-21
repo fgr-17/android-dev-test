46520c1b8dd36105c63af7e64005e5d4673c1ac1
Federico Roux <rouxfederico@gmail.com>
2024-05-21 10:37:22 -0300

    adding div function

--- KotlinBasics/app/src/main/java/eu/tutorials/kotlinbasics/Return.kt
+++ KotlinBasics/app/src/main/java/eu/tutorials/kotlinbasics/Return.kt
@@ -8,10 +8,16 @@ fun main() {
     val num2 = readln().toInt()
 
     println("$num1 + $num2 = " + add(num1, num2))
+    println("$num1 / $num2 = " + div(num1, num2))
 }
 
 
 fun add(num1: Int, num2: Int): Int {
     val result = num1 + num2
+    return result
+}
+
+fun div(num1: Int, num2: Int): Double {
+    val result = num1.toDouble() / num2.toDouble()
     return result
 }
