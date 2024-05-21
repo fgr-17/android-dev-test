e3921e41470b8deb2cfc4434a56e6e2a08b21531
Federico Roux <rouxfederico@gmail.com>
2024-05-21 10:12:52 -0300

    adding second param to function

--- KotlinBasics/app/src/main/java/eu/tutorials/kotlinbasics/Functions.kt
+++ KotlinBasics/app/src/main/java/eu/tutorials/kotlinbasics/Functions.kt
@@ -3,20 +3,23 @@ package eu.tutorials.kotlinbasics
 fun main(){
 
     // Call Function
-    makeCoffee(sugarCount = 1)
-    makeCoffee(sugarCount = 2)
-    makeCoffee(sugarCount = 30)
-    makeCoffee(sugarCount = 0)
-    makeCoffee(sugarCount = -1)
+    makeCoffee(sugarCount = 1, name = "fede")
+    makeCoffee(sugarCount = 2, name = "pepe")
+    makeCoffee(sugarCount = 30, "Santino")
+    makeCoffee(sugarCount = 0, "pepito")
+    makeCoffee(sugarCount = -1, "esculapio")
 }
 
 // Define Function
-fun makeCoffee(sugarCount : Int){
+fun makeCoffee(sugarCount : Int, name : String){
     if ( sugarCount == 1 ) {
-        println("Coffee with 1 spoon of sugar")
+        println("Coffee with 1 spoon of sugar for $name")
+    }
+    else if (sugarCount == 0) {
+        println("Coffee with no sugar for $name")
     }
     else {
-        println("Coffee with $sugarCount spoons of sugar")
+        println("Coffee with $sugarCount spoons of sugar for $name")
     }
 }
 
