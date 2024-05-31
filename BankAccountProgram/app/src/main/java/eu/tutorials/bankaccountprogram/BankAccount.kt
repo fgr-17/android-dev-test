25ee2d117d3e2cd65b2d9aa23edbef45fe23bc5d
Federico Roux <rouxfederico@gmail.com>
2024-05-31 15:52:34 -0300

    bank account test ok

--- BankAccountProgram/app/src/main/java/eu/tutorials/bankaccountprogram/BankAccount.kt
+++ BankAccountProgram/app/src/main/java/eu/tutorials/bankaccountprogram/BankAccount.kt
@@ -6,13 +6,13 @@ class BankAccount (var accountHolder: String, var balance: Double) {
 
     fun deposit(amount: Double){
         balance += amount
-        transactionHistory.add("$accountHolder deposited $$amount")
+        transactionHistory.add("$accountHolder deposited $$amount - balance: ${this.acctBalance()}")
     }
 
     fun withdraw(amount: Double){
         if (amount <= balance) {
             balance -= amount
-            transactionHistory.add("$accountHolder withdrew $$amount")
+            transactionHistory.add("$accountHolder withdrew $$amount - balance: ${this.acctBalance()}")
         }
         else {
             println("You don't have the funds to withdraw $$amount")
@@ -27,7 +27,10 @@ class BankAccount (var accountHolder: String, var balance: Double) {
     }
 
     fun displayAccountInfo(){
-        println("Account Holder: ${accountHolder}")
-        println("Balance: ${balance}")
+        println("Account Holder: ${accountHolder} - Balance: ${this.acctBalance()}")
+    }
+
+    fun acctBalance(): Double {
+        return balance
     }
 }
--- BankAccountProgram/app/src/main/java/eu/tutorials/bankaccountprogram/BankMain.kt
+++ BankAccountProgram/app/src/main/java/eu/tutorials/bankaccountprogram/BankMain.kt
@@ -2,12 +2,18 @@ package eu.tutorials.bankaccountprogram
 
 fun main() {
     val fedeBankAccount = BankAccount("Fede", 1302.20)
-
-    fedeBankAccount.displayAccountInfo()
-
     fedeBankAccount.deposit(300.4)
     fedeBankAccount.withdraw(10.4)
     fedeBankAccount.withdraw(44.1)
     fedeBankAccount.deposit(230.1)
     fedeBankAccount.displayTransactionHistory()
+    fedeBankAccount.displayAccountInfo()
+
+    val sarahBankAccount = BankAccount("Sarah", 300.0)
+    sarahBankAccount.deposit(100.0)
+    sarahBankAccount.withdraw(10.0)
+    sarahBankAccount.deposit(300.0)
+    sarahBankAccount.displayTransactionHistory()
+    sarahBankAccount.displayAccountInfo()
+
 }
