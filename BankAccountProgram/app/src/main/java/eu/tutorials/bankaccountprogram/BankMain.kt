4fb5cef6ef45cac11b301e48855236c68e8f0b1e
Federico Roux <rouxfederico@gmail.com>
2024-05-31 15:34:08 -0300

    bank account class

--- BankAccountProgram/app/src/main/java/eu/tutorials/bankaccountprogram/BankMain.kt
+++ BankAccountProgram/app/src/main/java/eu/tutorials/bankaccountprogram/BankMain.kt
@@ -4,4 +4,10 @@ fun main() {
     val fedeBankAccount = BankAccount("Fede", 1302.20)
 
     fedeBankAccount.displayAccountInfo()
+
+    fedeBankAccount.deposit(300.4)
+    fedeBankAccount.withdraw(10.4)
+    fedeBankAccount.withdraw(44.1)
+    fedeBankAccount.deposit(230.1)
+    fedeBankAccount.displayTransactionHistory()
 }
