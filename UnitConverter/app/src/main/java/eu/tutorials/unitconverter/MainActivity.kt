152764175a23eaf9524f6750dc129c456a6a5eeb
Federico Roux <rouxfederico@gmail.com>
2024-06-04 15:19:06 -0300

    button, click and toast added

--- UnitConverter/app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
@@ -1,11 +1,13 @@
 package eu.tutorials.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -13,6 +15,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import eu.tutorials.unitconverter.ui.theme.UnitConverterTheme
 
@@ -44,7 +47,12 @@ fun UnitConverter() {
             // what happens when it changes
         })
         Row {
-
+            val context = LocalContext.current
+            Button(onClick = { Toast.makeText(context,
+                                                "Thanks for clicking",
+                                                Toast.LENGTH_LONG).show() }) {
+                Text("Click")
+            }
         }
         Text("Result:")
     }
