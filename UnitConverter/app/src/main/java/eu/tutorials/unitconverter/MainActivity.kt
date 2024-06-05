85120468bbc96e9f07bec75b94f04c0e67d19706
Federico Roux <rouxfederico@gmail.com>
2024-06-05 08:08:33 -0300

    drop down menu

--- UnitConverter/app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
@@ -12,9 +12,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -67,13 +70,43 @@ fun UnitConverter() {
                     Text(text = "Select")
                     Icon(Icons.Default.ArrowDropDown,
                         contentDescription = "Arrow Down")
+                    DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                        DropdownMenuItem(
+                            text = { Text(text = "Centimeter") },
+                            onClick = { /*TODO*/ })
+                        DropdownMenuItem(
+                            text = { Text(text = "Meter") },
+                            onClick = { /*TODO*/ })
+                        DropdownMenuItem(
+                            text = { Text(text = "Feet") },
+                            onClick = { /*TODO*/ })
+                        DropdownMenuItem(
+                            text = { Text(text = "Milimeters") },
+                            onClick = { /*TODO*/ })
+                    }
                 }
+
             }
+            Spacer(modifier = Modifier.width(10.dp))
             Box {
                 Button(onClick = { /*TODO*/ }) {
                     Text(text = "Select")
                     Icon(Icons.Default.ArrowDropDown,
                         contentDescription = "Arrow Down")
+                    DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+                        DropdownMenuItem(
+                            text = { Text(text = "Centimeter") },
+                            onClick = { /*TODO*/ })
+                        DropdownMenuItem(
+                            text = { Text(text = "Meter") },
+                            onClick = { /*TODO*/ })
+                        DropdownMenuItem(
+                            text = { Text(text = "Feet") },
+                            onClick = { /*TODO*/ })
+                        DropdownMenuItem(
+                            text = { Text(text = "Milimeters") },
+                            onClick = { /*TODO*/ })
+                    }
                 }
             }
         }
